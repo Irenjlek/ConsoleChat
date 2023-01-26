ee2ead45505cdae7e45b71ee2dc90d140bd0dc4a
Roman-Kozhevnikov <fridaypost@yandex.ru>
2023-01-26 16:15:37 +0300

    created User definition

--- ConsoleChat/User.h
+++ ConsoleChat/User.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <ostream>
+#include <string>;
+
 class User
 {
+	std::string _name;
+	const std::string _login;
+	std::string _password;
+
+public:
+	User(std::string name, std::string login, std::string password);	
+	std::string& getName() { return _name; }
+	const std::string& getLogin() { return _login; }
+	std::string& getPassword() { return _password; }
+	friend std::ostream& operator<< (std::ostream&, User&);
+	//void changename(); // допник
+
 };
 
