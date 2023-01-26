d0a6f04e2fc165610a32086500e08bb0a653f09a
Irina <Iren_jlekapd@inbox.ru>
2023-01-26 20:37:15 +0300

    main elements and functions

--- ConsoleChat/Message.h
+++ ConsoleChat/Message.h
@@ -1,5 +1,22 @@
 #pragma once
+
+#include <string>
+
 class Message
 {
+private:
+	std::string _text;
+	std::string _sender;
+	std::string _recipient;
+	time_t _time;
+
+public:
+	Message(std::string text, std::string sender, std::string recipient, time_t time);
+
+	std::string getText();
+	std::string getSender();
+	std::string getRecipient();
+	time_t getTime();
+	friend std::ostream& operator<< (std::ostream&, Message&);
 };
 
