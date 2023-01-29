fa49661abede0a5c6828ed6507709a2d13ea76ff
Irina <Iren_jlekapd@inbox.ru>
2023-01-29 15:25:29 +0300

    functions for login

--- ConsoleChat/Chat.cpp
+++ ConsoleChat/Chat.cpp
@@ -13,14 +13,38 @@ Chat::~Chat()
 
 bool Chat::createNewUser(const std::string& name, const std::string& login, const std::string& password)
 {
-	for (User user: _users)
+	for (User* user: _users)
 	{
-		if (user.getLogin() == login) {
+		if (user->getLogin() == login) {
 			std::cout << "User with login " << login << " is already exists." << std::endl;
 			return false;
 		}
 	}
-	User newUser(name, login, password);
+	User* newUser = new User(name, login, password);
 	_users.push_back(newUser);
+	setActiveUser(std::make_shared<User>(*newUser));
+
+	return true;
+}
+
+void Chat::setActiveUser(const std::shared_ptr<User>& user)
+{
+	_activeUser = user;
+}
+
+bool Chat::login(std::string login, std::string password)
+{
+	for (User* user : _users)
+	{
+		if (user->getLogin() == login) {
+			if (user->getPassword() == password) {
+				setActiveUser(std::make_shared<User>(*user));
+			}
+			else {
+				std::cout << "Password is incorrect." << std::endl;
+				return false;
+			}
+		}
+	}
 	return true;
 }
