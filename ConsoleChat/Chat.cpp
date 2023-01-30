1538f0cbbd46391b046fc7c7be3acfdb3634a921
Roman-Kozhevnikov <fridaypost@yandex.ru>
2023-01-30 12:29:37 +0300

    added Message implement

--- ConsoleChat/Chat.cpp
+++ ConsoleChat/Chat.cpp
@@ -1,1 +1,80 @@
 #include "Chat.h"
+#include <iostream>
+
+Chat::Chat()
+{
+
+}
+
+Chat::~Chat()
+{
+
+}
+
+bool Chat::createNewUser(const std::string& name, const std::string& login, const std::string& password)
+{
+	for (User* user : _users)
+	{
+		if (user->getLogin() == login) {
+			std::cout << "User with login " << login << " is already exists." << std::endl;
+			return false;
+		}
+	}
+	User* newUser = new User(name, login, password);
+	_users.push_back(newUser);
+	setActiveUser(std::make_shared<User>(*newUser));
+
+	return true;
+}
+
+void Chat::setActiveUser(const std::shared_ptr<User>& user)
+{
+	_activeUser = user;
+}
+
+bool Chat::login(std::string login, std::string password)
+{
+	for (User* user : _users)
+	{
+		if (user->getLogin() == login) {
+			if (user->getPassword() == password) {
+				setActiveUser(std::make_shared<User>(*user));
+			}
+			else {
+				std::cout << "Password is incorrect." << std::endl;
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+
+void Chat::write(std::string text, std::shared_ptr<User>& recipient)
+{
+	time_t time_mess = time(&time_mess);		
+	
+	std::shared_ptr <Message> shp_mess(new Message(text, getActiveUser(_users).get()->getName(), 
+		                                         recipient.get()->getName(), time_mess));	
+	_messeges.push_back(*shp_mess);	
+	
+}
+
+void Chat::writeToAll(std::string text)
+{
+	time_t time_mess = time(&time_mess);
+	
+	
+	for (auto ricipient : _users)
+	{
+		std::shared_ptr <Message> shp_mess(new Message(text, getActiveUser(_users).get()->getName(),
+			(*ricipient).getName(), time_mess));
+		_messeges.push_back(*shp_mess);
+	}
+	
+}
+
+void Chat::showMenuAddMessege()
+{
+	std::cout << "¬ыберите режим получател€ : 1 - одному , 2 - всем (All) \n";
+}
--- ConsoleChat/Chat.h
+++ ConsoleChat/Chat.h
@@ -1,22 +1,43 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <memory>
+#include <iostream>
 #include "User.h"
 #include "Message.h"
-#include <vector>
-#include <memory>
+
 class Chat
 {
-	std::vector <User> _users;
+private:
+	std::vector<User*> _users;
 	std::vector <Message> _messeges;
-    std::shared_ptr <User> _activeUser;
+	std::shared_ptr <User> _activeUser;
+
 public:
 	Chat();
-	void setActiveUser(const std::shared_ptr<User> &);
-	bool login(std::string login, std::string password, std::string name);
-	void write(std::string text, std::shared_ptr<User> &);
-	void writeAll(std::string text);
-	std::shared_ptr <User> getActiveUser(const std::vector<User> &);
-	std::shared_ptr <User> getUser(const std::vector<User> &);
+	~Chat();
+
+	bool createNewUser(const std::string& name, const std::string& login, const std::string& password);
+	void setActiveUser(const std::shared_ptr<User>& user);
+	bool login(std::string login, std::string password);
+	void write(std::string text, std::shared_ptr<User>&);
+	void writeToAll(std::string text);
+	std::shared_ptr <User> getActiveUser(const std::vector<User*>&);
+	std::shared_ptr <User> getUser(const std::vector<User*>&);
 	void showMenuAddUser();
 	void showMenuAddMessege();
-};
 
+	//temp for debug
+	void showActive() {
+		std::cout << "showactive" << std::endl;
+		std::cout << _activeUser->getName() << std::endl;
+	}
+
+	void showAll() {
+		std::cout << "showall" << std::endl;
+		for (User* user : _users)
+		{
+			std::cout << user->getName() << " " << std::endl;
+		}
+	}
+};
